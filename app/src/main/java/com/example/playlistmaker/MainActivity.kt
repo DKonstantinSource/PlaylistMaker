72450949a717c3e32c95b8be3e03c515679c7e40
Konstantin <fluxgerax@gmail.com>
2024-08-08 21:16:28 +0500

    Second commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val searchView = findViewById<Button>(R.id.first_button)
+        val mediaLibraryView = findViewById<Button>(R.id.second_button)
+        val settingView = findViewById<Button>(R.id.third_button)
+
+        val searchViewDark = findViewById<Button>(R.id.first_button_dark)
+        val mediaLibraryViewDark = findViewById<Button>(R.id.second_button_dark)
+        val settingViewDark = findViewById<Button>(R.id.third_button_dark)
+
+        searchView.setOnClickListener {
+            val displaySettingsActivity = Intent(this, SearchActivity::class.java)
+            startActivity(displaySettingsActivity)
         }
+        mediaLibraryView.setOnClickListener {
+            val displaySettingsActivity = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(displaySettingsActivity)
+        }
+        settingView.setOnClickListener {
+            val displaySettingsActivity = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySettingsActivity)
+        }
+
+
     }
 }
+
+
+
+
+
+//ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//insets
+
+//}
